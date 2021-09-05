24d2ec8dbd0af42222c307c86fdbb2141db40821
unknown <da4adindu@gmail.com>
2021-09-05 21:22:27 +0100

    picture bug

--- index.php
+++ index.php
@@ -2,4 +2,11 @@
 
 include_once("index.html");
 
+
+$file = 'about-me.jpg';
+$type = 'images/jpeg';
+header('Content-Type:'.$type);
+header('Content-Length: ' . filesize($file));
+readfile($file);
+
 ?>
