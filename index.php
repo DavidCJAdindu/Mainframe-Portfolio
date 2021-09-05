5675e069d262ad41c515bb2cfef41934ccfc9093
unknown <da4adindu@gmail.com>
2021-09-05 21:27:38 +0100

    still working on bugs

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 include_once("index.html");
 
 
-$file = 'about-me.jpg';
+$file = 'images/about-me.jpg';
 $type = 'image/jpg';
 header('Content-Type:'.$type);
 header('Content-Length: ' . filesize($file));
