d602bf42be7d38491f5f3a559b9cca56ca02c3b9
unknown <da4adindu@gmail.com>
2021-09-06 15:26:03 +0100

    minor bug

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
 
 include_once("root/index.html");
+include_once("root/style.css");
+include_once("root/script.js");
 
 
 $file = 'images/about-me.jpg';
 $type = 'image/jpg';
 header('Content-Type:'.$type);
 header('Content-Length: ' . filesize($file));
+header("Content-type: text/css; charset: UTF-8");
+header("Content-type: text/js; charset: UTF-8");
 readfile($file);
 
 ?>
